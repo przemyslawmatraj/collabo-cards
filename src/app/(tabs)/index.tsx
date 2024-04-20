87c6ca71891d3fb0f29edba6763fc13edb505c6a
przemyslawmatraj <Przemyslaw.Matraj@microsoft.wsei.edu.pl>
2024-04-20 19:59:13 +0200

    add ts path aliases, add nativewind

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,36 +1,15 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "@/src/components/EditScreenInfo";
-import { Text, View } from "@/src/components/Themed";
+import EditScreenInfo from "@components/EditScreenInfo";
+import { Text, View } from "@components/Themed";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
+    <View tw="flex flex-col items-center justify-center h-screen">
+      <Text tw="text-yellow-500 mx-auto">Tab One</Text>
+      <View lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
